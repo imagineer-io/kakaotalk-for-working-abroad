fe749f2388b13a2d971bac006c3bad70ef02b091
Minsoo Kwak <marcosdeseul@gmail.com>
2017-03-30 15:18:34 +0800

    Add auth layout and controller

---
+++ imagineertalk/AuthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AuthViewController.swift
+//  imagineertalk
+//
+//  Created by marco on 3/30/17.
+//  Copyright © 2017 imagineer. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

8b4bdcbca3dead2f36e39aa5de8e8c3b13595149
Minsoo Kwak <marcosdeseul@gmail.com>
2017-04-06 17:55:24 +0800

    Change MainViewController with table

--- imagineertalk/MainViewController.swift
+++ imagineertalk/MainViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import Firebase
 import FirebaseAuth
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var ref: FIRDatabaseReference!
+    var users: [FIRDataSnapshot]! = []
+    var _refHandle: FIRDatabaseHandle!
+    
+    @IBOutlet weak var userTableView: UITableView!
     @IBOutlet weak var emailLabel: UILabel!
     var handle:FIRAuthStateDidChangeListenerHandle?
-    
     
     @IBAction func logoutButtonPressed(_ sender: Any) {
         do {
@@ -34,6 +39,7 @@ class MainViewController: UIViewController {
             }
         }
         // Do any additional setup after loading the view.
+        configureDatabase()
     }
 
     /*
@@ -45,5 +51,39 @@ class MainViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.userTableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        // Unpack message from Firebase DataSnapshot
+        let userSnapshot: FIRDataSnapshot! = self.users[indexPath.row]
+        guard let user = userSnapshot.value as? [String:String] else { return cell }
+        let text = user["email"] ?? "[email]"
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    deinit {
+        if let refHandle = _refHandle {
+            self.ref.child("users").removeObserver(withHandle: refHandle)
+        }
+    }
+    
+    func configureDatabase() {
+        ref = FIRDatabase.database().reference()
+        // Listen for new messages in the Firebase database
+        _refHandle = self.ref.child("users")
+            //            .queryOrdered(byChild: "text")
+            //            .queryEqual(toValue: "Abc")
+            .observe(.childAdded, with: { [weak self] (snapshot) -> Void in
+                guard let strongSelf = self else { return }
+                strongSelf.users.append(snapshot)
+                strongSelf.userTableView.insertRows(at: [IndexPath(row: strongSelf.users.count-1, section: 0)], with: .automatic)
+            })
+    }
+
 
 }
